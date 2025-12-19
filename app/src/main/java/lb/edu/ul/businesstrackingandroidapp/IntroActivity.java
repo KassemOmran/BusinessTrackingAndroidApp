82ef10ed253f8032803d3dacbc49502c86e9c0d8
KassemOmran <78233161+KassemOmran@users.noreply.github.com>
2025-12-19 15:31:53 +0200

    dashboard cards

---
+++ app/src/main/java/lb/edu/ul/businesstrackingandroidapp/IntroActivity.java
@@ -0,0 +1,41 @@
+package lb.edu.ul.businesstrackingandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // close the splash screen so the user can’t go back to it
+            }
+        },2000);
+
+
+    }
+
+
+
+}
